246e1f548e075527e42ca8579095667765a33405
leonseki <leonstokyo@gmail.com>
2024-04-05 22:59:13 +0900

    init

---
+++ src/main/java/jp/tokyo/leon/zeus/gateway/ZeusGatewayApplication.java
@@ -0,0 +1,18 @@
+package jp.tokyo.leon.zeus.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author leon
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ZeusGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZeusGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jp/tokyo/leon/zeus/gateway/ZeusGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.tokyo.leon.zeus.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeusGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
